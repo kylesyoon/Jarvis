d881eeea765399af806c81f3228c1a237b2fc93e
Kyle Yoon <kyle.yoon@vokalinteractive.com>
2015-03-19 14:29:25 -0500

    Initial Commit

---
+++ Jarvis/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jarvis
+//
+//  Created by Kyle Yoon on 3/19/15.
+//  Copyright (c) 2015 Kyle Yoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
